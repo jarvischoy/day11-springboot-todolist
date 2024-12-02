a90f24482892b187bcade8054d2f4b1e6ac4db8d
Jarvis Choy <choyja2@oocl.com>
2024-12-02 11:36:10 +0800

    init

---
+++ src/main/java/com/todo/day11springboottodolist/Day11SpringbootTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.todo.day11springboottodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11SpringbootTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day11SpringbootTodolistApplication.class, args);
+    }
+
+}
